1accc10d3a7b659e4006d277d6bb77addf9e6e5c
mbailey672 <71410092+mbailey672@users.noreply.github.com>
2021-08-26 15:39:53 -0600

    added O(N) solution

--- two_sum.go
+++ two_sum.go
@@ -21,7 +21,25 @@ func twoSum(nums []int, target int) []int {
 	return solution
 }
 
+func twoSumHashmap(nums []int, target int) []int {
+	cache := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		compliment := target - nums[i]
+		if _, found := cache[compliment]; found {
+			if cache[compliment] != i {
+				solution := []int{i, cache[compliment]}
+				return solution
+			}
+		}
+		cache[nums[i]] = i
+	}
+	solution := make([]int, 2)
+	return solution
+}
+
 func main() {
 	numArray := []int{2, 7, 11, 15}
 	fmt.Println(twoSum(numArray, 9))
+	fmt.Println(twoSumHashmap(numArray, 9))
 }
