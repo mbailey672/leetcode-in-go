9d9b6e350d27f91e4b6ba2cbfd336afebf4ba00f
mbailey672 <71410092+mbailey672@users.noreply.github.com>
2021-08-27 15:10:10 -0600

    #121 (E)

---
+++ best_time_to_buy_sell.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+//O(n^2) brute force
+func maxProfit(prices []int) int {
+	maximizedProfit := 0
+	for i := 0; i < len(prices); i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if (prices[j] - prices[i]) > maximizedProfit {
+				maximizedProfit = prices[j] - prices[i]
+			}
+		}
+	}
+	return maximizedProfit
+}
+
+func maxProfitOptimized(prices []int) int {
+	smallestPrice := prices[0]
+	maximizedProfit := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < smallestPrice {
+			smallestPrice = prices[i]
+		} else if (prices[i] - smallestPrice) > maximizedProfit {
+			maximizedProfit = prices[i] - smallestPrice
+		}
+	}
+	return maximizedProfit
+}
+
+func main() {
+	numArray := []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(numArray))
+	fmt.Println(maxProfitOptimized(numArray))
+}
